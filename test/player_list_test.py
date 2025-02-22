953e27d6f2b1b411a5e5b347398834863e242a7c
Elisha <elishamutang@outlook.com>
2025-02-22 23:06:05 +0800

    Added unit tests to test PlayerList class.

---
+++ test/player_list_test.py
@@ -0,0 +1,32 @@
+import unittest
+from app.player_list import PlayerList
+from app.player_node import PlayerNode
+from app.player import Player
+
+class TestPlayerList(unittest.TestCase):
+
+    def setUp(self):
+        self.player_list = PlayerList()
+
+    def test_add_to_empty_list(self):
+        test_player_one = Player(123, 'Joe')
+        test_player_one_node = PlayerNode(test_player_one)
+
+        self.player_list.shift(test_player_one_node)
+
+        self.assertEqual(self.player_list.is_empty, False)
+        self.assertEqual(self.player_list.head.key, test_player_one_node.key)
+
+    def test_add_to_non_empty_list(self):
+        test_player_one = Player(123, 'Joe')
+        test_player_one_node = PlayerNode(test_player_one)
+
+        self.player_list.shift(test_player_one_node)
+
+        test_player_two = Player(456, 'Smith')
+        test_player_two_node = PlayerNode(test_player_two)
+
+        self.player_list.shift(test_player_two_node)
+
+        self.assertEqual(len(self.player_list), 2)
+        self.assertEqual(self.player_list.head.key, 456)
