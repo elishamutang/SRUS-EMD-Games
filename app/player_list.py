e2bddaf45c8ca1ac6e85a56e8c1d46e0ad1de6de
Elisha <elishamutang@outlook.com>
2025-02-22 23:06:36 +0800

    Added class properties and method to add new node to head of list.

--- app/player_list.py
+++ app/player_list.py
@@ -2,3 +2,45 @@ class PlayerList:
 
     def __init__(self):
         self._head = None
+
+    def __len__(self):
+        length = 0
+        current_node = self.head
+
+        if current_node is None:
+            return length
+        else:
+            while current_node is not None:
+                current_node = current_node.next
+                length += 1
+
+            return length
+
+    @property
+    def is_empty(self):
+        """Check if list is empty"""
+        return len(self) == 0
+
+    @property
+    def head(self):
+        """Returns head of list"""
+        return self._head
+
+    @head.setter
+    def head(self, head):
+        """Set new head"""
+        self._head = head
+
+    # Insert new node at head of list.
+    def shift(self, node):
+        if self.is_empty:
+            self.head = node
+        else:
+            current_node = self.head
+
+            while current_node.prev is not None:
+                current_node = current_node.prev
+
+            self.head = node
+            node.next = current_node
+            current_node.prev = self.head
