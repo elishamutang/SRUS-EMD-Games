192e89c6e994f5ce9420b6012c9baf8b961d21ef
Elisha <elishamutang@outlook.com>
2025-02-22 19:00:33 +0800

    Added PlayerNode class.

---
+++ app/player_node.py
@@ -0,0 +1,36 @@
+class PlayerNode:
+
+    def __init__(self, player):
+        self._player = player
+        self._next = None
+        self._prev = None
+
+    def __str__(self):
+        return (f"Previous player: {self.prev.uid if self.prev is not None else None},"
+                f"\nCurrent player: {self.key},"
+                f"\nNext player: {self.next.uid if self.next is not None else None}")
+
+    @property
+    def key(self):
+        """Return player ID"""
+        return self._player.uid
+
+    @property
+    def next(self):
+        """Get next Player"""
+        return self._next
+
+    @next.setter
+    def next(self, player):
+        """Set next player"""
+        self._next = player
+
+    @property
+    def prev(self):
+        """Get previous Player"""
+        return self._prev
+
+    @prev.setter
+    def prev(self, player):
+        """Set previous player"""
+        self._prev = player
