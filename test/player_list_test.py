ae04bf3042493959ca6c4e3b61b31f8b6bf318b1
Elisha <elishamutang@outlook.com>
2025-02-23 22:45:13 +0800

    Updated tests.

--- test/player_list_test.py
+++ test/player_list_test.py
@@ -8,62 +8,54 @@ class TestPlayerList(unittest.TestCase):
     def setUp(self):
         self.player_list = PlayerList()
 
-    def test_add_to_head_on_empty_list(self):
         test_player_one = Player(123, 'Joe')
-        test_player_one_node = PlayerNode(test_player_one)
-
-        self.player_list.shift(test_player_one_node)
-
-        self.assertEqual(self.player_list.is_empty, False)
-        self.assertEqual(self.player_list.head.key, test_player_one_node.key)
-
-    def test_add_to_head_on_non_empty_list(self):
-        test_player_one = Player(123, 'Joe')
-        test_player_one_node = PlayerNode(test_player_one)
-
-        self.player_list.shift(test_player_one_node)
+        self.test_player_one_node = PlayerNode(test_player_one)
 
         test_player_two = Player(456, 'Smith')
-        test_player_two_node = PlayerNode(test_player_two)
+        self.test_player_two_node = PlayerNode(test_player_two)
 
-        self.player_list.shift(test_player_two_node)
+        test_player_three = Player(789, 'Brett')
+        self.test_player_three_node = PlayerNode(test_player_three)
+
+        test_player_four = Player(431, 'John')
+        self.test_player_four_node = PlayerNode(test_player_four)
+
+        test_player_five = Player(856, 'Sarah')
+        self.test_player_five_node = PlayerNode(test_player_five)
+
+        test_player_six = Player(223, 'Oli')
+        self.test_player_six_node = PlayerNode(test_player_six)
+
+    def test_add_to_head_on_empty_list(self):
+        self.player_list.shift(self.test_player_one_node)
+
+        self.assertEqual(self.player_list.is_empty, False)
+        self.assertEqual(self.player_list.head.key, self.test_player_one_node.key)
+
+    def test_add_to_head_on_non_empty_list(self):
+        self.player_list.shift(self.test_player_one_node)
+        self.player_list.shift(self.test_player_two_node)
 
         self.assertEqual(len(self.player_list), 2)
         self.assertEqual(self.player_list.head.key, 456)
         self.assertEqual(self.player_list.tail.key, 123)
 
     def test_add_to_tail_on_empty_list(self):
-        test_player_one = Player(123, 'Joe')
-        test_player_one_node = PlayerNode(test_player_one)
-
-        self.player_list.push(test_player_one_node)
+        self.player_list.push(self.test_player_one_node)
 
         self.assertEqual(self.player_list.is_empty, False)
         self.assertEqual(self.player_list.head.key, 123)
 
     def test_add_to_tail_on_non_empty_list(self):
-        test_player_one = Player(123, 'Joe')
-        test_player_one_node = PlayerNode(test_player_one)
-
-        self.player_list.push(test_player_one_node)
-
-        test_player_two = Player(456, 'Smith')
-        test_player_two_node = PlayerNode(test_player_two)
-
-        self.player_list.push(test_player_two_node)
+        self.player_list.push(self.test_player_one_node)
+        self.player_list.push(self.test_player_two_node)
 
         self.assertEqual(len(self.player_list), 2)
         self.assertEqual(self.player_list.tail.key, 456)
 
     def test_remove_item_from_head(self):
-        test_player_one = Player(123, 'Joe')
-        test_player_one_node = PlayerNode(test_player_one)
-
-        test_player_two = Player(456, 'Smith')
-        test_player_two_node = PlayerNode(test_player_two)
-
-        self.player_list.push(test_player_one_node)
-        self.player_list.push(test_player_two_node)
+        self.player_list.push(self.test_player_one_node)
+        self.player_list.push(self.test_player_two_node)
 
         self.player_list.unshift()
 
@@ -77,18 +69,9 @@ class TestPlayerList(unittest.TestCase):
         self.assertEqual(self.player_list.unshift(), 'The list is empty.')
 
     def test_remove_item_from_tail(self):
-        test_player_one = Player(123, 'Joe')
-        test_player_one_node = PlayerNode(test_player_one)
-
-        test_player_two = Player(456, 'Smith')
-        test_player_two_node = PlayerNode(test_player_two)
-
-        test_player_three = Player(789, 'Brett')
-        test_player_three_node = PlayerNode(test_player_three)
-
-        self.player_list.push(test_player_one_node)
-        self.player_list.push(test_player_two_node)
-        self.player_list.push(test_player_three_node)
+        self.player_list.push(self.test_player_one_node)
+        self.player_list.push(self.test_player_two_node)
+        self.player_list.push(self.test_player_three_node)
 
         self.player_list.pop()
 
