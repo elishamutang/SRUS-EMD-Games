552fe4ac74e69cfab9f70feb117bb04af928619a
Elisha <elishamutang@outlook.com>
2025-03-05 20:49:37 +0800

    Added type hinting.

--- app/player_hashmap.py
+++ app/player_hashmap.py
@@ -7,16 +7,16 @@ class PlayerHashMap:
     def __init__(self):
         self.hashmap = [PlayerList() for i in range(0, self.SIZE)]
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: str) -> None:
         pass
 
-    def __setitem__(self, key, name):
+    def __setitem__(self, key: str, name: str) -> None:
         pass
 
     def __len__(self):
         pass
 
-    def __delitem__(self, key):
+    def __delitem__(self, key: str) -> None:
         pass
 
 
