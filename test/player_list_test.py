2a982c4140df1dceea3f28200de391b27d843e75
elishamutang <elishamutang@outlook.com>
2025-03-05 12:31:04 +0800

    Amended tests.

--- test/player_list_test.py
+++ test/player_list_test.py
@@ -3,27 +3,28 @@ from app.player_list import PlayerList
 from app.player_node import PlayerNode
 from app.player import Player
 
+
 class TestPlayerList(unittest.TestCase):
 
     def setUp(self):
         self.player_list = PlayerList()
 
-        test_player_one = Player(123, 'Joe')
+        test_player_one = Player('123', 'Joe')
         self.test_player_one_node = PlayerNode(test_player_one)
 
-        test_player_two = Player(456, 'Smith')
+        test_player_two = Player('456', 'Smith')
         self.test_player_two_node = PlayerNode(test_player_two)
 
-        test_player_three = Player(789, 'Brett')
+        test_player_three = Player('789', 'Brett')
         self.test_player_three_node = PlayerNode(test_player_three)
 
-        test_player_four = Player(431, 'John')
+        test_player_four = Player('431', 'John')
         self.test_player_four_node = PlayerNode(test_player_four)
 
-        test_player_five = Player(856, 'Sarah')
+        test_player_five = Player('856', 'Sarah')
         self.test_player_five_node = PlayerNode(test_player_five)
 
-        test_player_six = Player(223, 'Oli')
+        test_player_six = Player('223', 'Oli')
         self.test_player_six_node = PlayerNode(test_player_six)
 
     def test_add_to_head_on_empty_list(self):
@@ -38,21 +39,21 @@ class TestPlayerList(unittest.TestCase):
         self.player_list.shift(self.test_player_two_node)
 
         self.assertEqual(len(self.player_list), 2)
-        self.assertEqual(self.player_list.head.key, 456)
-        self.assertEqual(self.player_list.tail.key, 123)
+        self.assertEqual(self.player_list.head.key, '456')
+        self.assertEqual(self.player_list.tail.key, '123')
 
     def test_add_to_tail_on_empty_list(self):
         self.player_list.push(self.test_player_one_node)
 
         self.assertEqual(self.player_list.is_empty, False)
-        self.assertEqual(self.player_list.head.key, 123)
+        self.assertEqual(self.player_list.head.key, '123')
 
     def test_add_to_tail_on_non_empty_list(self):
         self.player_list.push(self.test_player_one_node)
         self.player_list.push(self.test_player_two_node)
 
         self.assertEqual(len(self.player_list), 2)
-        self.assertEqual(self.player_list.tail.key, 456)
+        self.assertEqual(self.player_list.tail.key, '456')
 
     def test_remove_item_from_head(self):
         self.player_list.push(self.test_player_one_node)
@@ -61,7 +62,7 @@ class TestPlayerList(unittest.TestCase):
         self.player_list.unshift()
 
         self.assertEqual(len(self.player_list), 1)
-        self.assertEqual(self.player_list.head.key, 456)
+        self.assertEqual(self.player_list.head.key, '456')
 
         self.player_list.unshift()
 
@@ -79,12 +80,12 @@ class TestPlayerList(unittest.TestCase):
         self.player_list.pop()
 
         self.assertEqual(len(self.player_list), 2)
-        self.assertEqual(self.player_list.tail.key, 456)
+        self.assertEqual(self.player_list.tail.key, '456')
 
         self.player_list.pop()
 
         self.assertEqual(len(self.player_list), 1)
-        self.assertEqual(self.player_list.head.key, 123)
+        self.assertEqual(self.player_list.head.key, '123')
 
         self.player_list.pop()
 
