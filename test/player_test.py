49cd2cb560fcbf4cb023642d0cde8404a7bc38a1
elishamutang <elishamutang@outlook.com>
2025-03-05 12:31:04 +0800

    Amended tests.

--- test/player_test.py
+++ test/player_test.py
@@ -1,13 +1,14 @@
 import unittest
 from app.player import Player
 
+
 class TestPlayer(unittest.TestCase):
 
     def setUp(self):
-        self.player = Player(123, 'John')
+        self.player = Player('123', 'John')
 
     def test_user_id(self):
-        self.assertEqual(self.player.uid, 123)
+        self.assertEqual(self.player.uid, '123')
 
     def test_user_name(self):
         self.assertEqual(self.player.name, 'John')
