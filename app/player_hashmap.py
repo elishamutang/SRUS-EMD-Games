8f6c6617ddc688835c7097ffe2bc3d97bd917c47
elishamutang <elishamutang@outlook.com>
2025-03-12 20:14:39 +0800

    Amended set item dunder method.

--- app/player_hashmap.py
+++ app/player_hashmap.py
@@ -10,7 +10,7 @@ class PlayerHashMap:
         self.hashmap = [PlayerList() for i in range(0, self.SIZE)]
 
     def __getitem__(self, key: str | Player) -> int:
-        # Find index based on Player hash.
+        # Return player index based on Player hash.
         if isinstance(key, Player):
             player_index = hash(key) % self.SIZE
         else:
@@ -24,32 +24,34 @@ class PlayerHashMap:
         new_player_node = PlayerNode(new_player)
 
         # Find index of PlayerList where new player will be placed.
-        player_list_index = hash(new_player) % self.SIZE
+        player_list_index = self[new_player] # NOT SURE IF THIS IS GOOD PRACTICE OR NOT.
 
         # Access PlayerList at player_list_index and place new_player_node if it does not already exist.
         player_list = self.hashmap[player_list_index]
 
-        if player_list.is_empty:
-            player_list.push(new_player_node)
-        else:
-            # If new_player already exists, overwrite it
-            current = player_list.head
+        # Check if player already exists.
 
-            while current.value != key and current is not None:
-                current = current.next
-
-            current.value = key
+        # And push to player list.
+        player_list.push(new_player_node)
 
     def __len__(self):
-        pass
+        length = 0
+
+        for i in range(self.SIZE):
+            # print(len(self.hashmap[i]))
+            length += len(self.hashmap[i])
+
+        return length
 
     def __delitem__(self, key: str) -> None:
         pass
 
 
 test_hashmap = PlayerHashMap()
-test_player = Player('123', 'John')
-
 test_hashmap['123'] = 'John'
-print(test_hashmap['123'])
-print(test_hashmap[test_player])
+test_hashmap['456'] = 'Jane'
+
+print(len(test_hashmap))
+for hashmap in test_hashmap.hashmap:
+    if len(hashmap) >= 1:
+        print(hashmap.display())
