1a58bb38c37a91709ccf312165eebc59dec2e00c
elishamutang <elishamutang@outlook.com>
2025-04-01 11:51:38 +0800

    Initial test for sorting players.

---
+++ test/test_player.py
@@ -0,0 +1,18 @@
+import unittest
+from app.player import Player
+
+
+class TestPlayerSort(unittest.TestCase):
+
+    def test_sort_players(self):
+        players = [Player(player_id='01', name='Alice', score=10),
+                   Player(player_id='02', name='Bob', score=5),
+                   Player(player_id='03', name='Charlie', score=15)]
+
+        sorted_players = sorted(players)
+
+        manually_sorted_players = [Player(player_id='02', name='Bob', score=5),
+                                   Player(player_id='01', name='Alice', score=10),
+                                   Player(player_id='03', name='Charlie', score=15)]
+
+        self.assertListEqual(sorted_players, manually_sorted_players)
