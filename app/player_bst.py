7ec9783d522ad47ed492bc4c72d52a833eca17d7
elishamutang <elishamutang@outlook.com>
2025-05-06 18:00:05 +0800

    update: Completed search method.

--- app/player_bst.py
+++ app/player_bst.py
@@ -7,12 +7,12 @@ class PlayerBST:
         self._root = None
 
     @property
-    def root(self):
+    def root(self) -> PlayerBNode:
         """Return root node of PlayerBST."""
         return self._root
 
     @root.setter
-    def root(self, node):
+    def root(self, node: PlayerBNode):
         self._root = node
 
     def insert(self, player: Player) -> PlayerBNode:
@@ -55,22 +55,46 @@ class PlayerBST:
         return self.root
 
 
-    def search(self, name: str) -> str:
+    def search(self, name: str) -> Player | None:
         """
-        Search for player name and returns it.
+        Search for player name and return player.
 
         Args:
             name (str): Player name.
 
         Returns:
-            Player name (str).
+            Player.
         """
-        pass
+
+        print(f"Searching for {name}")
+
+        if self.root is None:
+            return None
+
+        if self.root.key == name:
+            return self.root.player
+
+        if name < self.root.key:
+
+            left_subtree = PlayerBST()
+            left_subtree.root = self.root.left
+            print(f"Going down left subtree: {left_subtree.root}")
+
+            player = left_subtree.search(name)
+            print(f"{player} found in left subtree of {self.root}")
+            return player
+        elif name > self.root.key:
+
+            right_subtree = PlayerBST()
+            right_subtree.root = self.root.right
+            print(f"Going down right subtree: {right_subtree.root}")
+
+            player = right_subtree.search(name)
+            print(f"{player} found in right subtree of {self.root}")
+            return player
 
     def __str__(self):
         return f"{self.root}"
-
-
 
 
 # test = PlayerBST()
@@ -78,17 +102,20 @@ class PlayerBST:
 # player_two = Player('2', 'Jack' ,2)
 # player_three = Player('3', 'Koala', 3)
 # player_four = Player('5', 'Jake', 5)
-# player_five = Player('6', 'Lauren', 20)
-# player_six = Player('7', 'Lauren', 10)
-#
-#
-#
+# # player_five = Player('6', 'Lauren', 20)
+# # player_six = Player('7', 'Lauren', 10)
+# #
+# #
+# #
 # test.insert(player_one)
 # test.insert(player_two)
 # test.insert(player_three)
 # test.insert(player_four)
-# test.insert(player_five)
-# test.insert(player_six)
-# print(test.root.right.right.player)
+# # test.insert(player_five)
+# # test.insert(player_six)
+# # print(test.root.right.right.player)
+#
+# print(test)
+# print(test.search('Chicken'))
 
 
